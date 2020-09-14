8676dfc9a53118e1e88515b242d5bf7d73a0eeda
Aleksander <Zakharov.owl@gmail.com>
2020-09-14 22:47:31 +0300

    add hide and show

--- src/core/component.js
+++ src/core/component.js
@@ -1,5 +1,19 @@
 export class Component{
     constructor(id){
         this.$el = document.getElementById(id) //для скорости использовал getElementById
+        this.init()
     }
+
+    init(){}
+
+    hide(){
+        this.$el.classList.add('hide')
+    }
+
+    show(){
+        this.$el.classList.remove('hide')
+    }
+
 }
+
+
