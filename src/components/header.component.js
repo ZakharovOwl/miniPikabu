52f1783e5adb1d70b05df901b31bd52f2b84ba2c
Aleksander <Zakharov.owl@gmail.com>
2020-09-20 21:16:10 +0300

    feat: fix step 7

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -6,7 +6,7 @@ export class HeaderComponent extends Component {
   }
 
   init() {
-    if(localStorage.getItem('visited')){
+    if (localStorage.getItem('visited')) {
       this.hide()
     }
     const btn = this.$el.querySelector('.js-header-start')
@@ -15,6 +15,6 @@ export class HeaderComponent extends Component {
 }
 
 function buttonHandler() {
-  localStorage.setItem('visited',JSON.stringify(true))
+  localStorage.setItem('visited', JSON.stringify(true))
   this.hide()
 }
